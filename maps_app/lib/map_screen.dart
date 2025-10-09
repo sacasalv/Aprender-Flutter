51973a269d0aaacf9c581329029e8925ee0b564c
Santiago <sacasalv@gmail.com>
2025-10-09 23:22:41 +0200

    App map

--- maps_app/lib/map_screen.dart
+++ maps_app/lib/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,10 +9,56 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? _controller;
+
+  final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(28.490295, -16.326535),
+    zoom: 12,
+  );
+
+  final Set<Marker> _markers = {
+    Marker(markerId: MarkerId("Pepito"), position: LatLng(28.490295, -16.326535), infoWindow: InfoWindow(title: "Mi restaurante"))
+  };
+
+  void addMarker(LatLng latLong) async{
+    TextEditingController _textController = TextEditingController();
+
+    String? title = await showDialog<String>(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("Añade un titulo"),
+        content: TextField(
+          controller: _textController,
+          decoration: InputDecoration(hintText: "Restaurante..."),
+        ),
+        actions: [
+          TextButton(onPressed: () => Navigator.of(context).pop(null), child: Text("Cancelar")),
+          TextButton(onPressed: () => Navigator.of(context).pop(_textController.text), child: Text("Guardar")),
+        ],
+      );
+    });
+
+    if(title != null && title.isNotEmpty){
+      setState(() {
+        _markers.add(
+          Marker(markerId: MarkerId(latLong.toString()), position: latLong, infoWindow: InfoWindow(title: title))
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text("Mapa guapo")),
+      body: GoogleMap(
+        initialCameraPosition: _initialPosition,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+        mapType: MapType.normal,
+        markers: _markers,
+        onTap: (latLong) => addMarker(latLong),
+      ),
     );
   }
 }
