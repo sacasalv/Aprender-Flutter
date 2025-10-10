fd39f9b985cee0dd955be2326cb3b62bcf408bd2
Santiago <sacasalv@gmail.com>
2025-10-10 16:55:57 +0200

    Configuracion para el darkmode

--- settings_app/lib/main.dart
+++ settings_app/lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:settings_app/preferences_keys.dart';
 import 'package:settings_app/settings_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Se encarga de que todo ha sido inicializado para hacer preferences
+ final prefs = await SharedPreferences.getInstance();
+ bool darkMode = prefs.getBool(PreferencesKeys.darkMode) ?? false;
+  runApp(MainApp(isDarkMode: darkMode,));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final bool isDarkMode;
+
+  const MainApp({super.key, required this.isDarkMode});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(), //Opciones para hacer esto de forma dinamica 1.Moverlo a settings_screen 2. Hacerlo con Future Builder 3. Hacerlo antes de que se llame al MainApp(no muy recomendablle)
+      home: Scaffold( 
         appBar: AppBar( title: Text("Configuracion"),),
         body: SettingsScreen(),
       ),
